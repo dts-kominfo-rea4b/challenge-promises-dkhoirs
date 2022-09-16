21d0e4876cf324ab6d6f945589a76a2c94468ca2
khoirudin khoir <dkhoirs@gmail.com>
2022-09-16 15:54:33 +0700

    update promise.js M. Khoiruddin

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const hasilpromiseTheaterIXX = await promiseTheaterIXX();
+  const hasilpromiseTheaterVGC = await promiseTheaterVGC();
+  const hasilnonton = [...hasilpromiseTheaterIXX, ...hasilpromiseTheaterVGC];
+  const filterhasil = hasilnonton.filter((e) => {
+    return e.hasil == emosi;
+  });
+  return filterhasil.length;
+};
 
 module.exports = {
   promiseOutput,
